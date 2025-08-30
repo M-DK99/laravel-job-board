70edeafd7b00895736f40383b5df00f52c75f71f
M-DK99 <ahmad.bakkar.1555@gmail.com>
2025-08-30 14:55:13 +0300

    Added The Database From Project

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,7 +7,7 @@ class Job
         return [
             ['title' => 'Job 1', 'salary' => '10000'],
             ['title' => 'Job 2', 'salary' => '20000'],
-            ['title' => 'Job 3', 'salary' => '3000'],
+            ['title' => 'Job 3', 'salary' => '30000'],
         ];
     }
 }
